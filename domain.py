654fdc183947842a0b2c1a4c32751859b783d439
razi <razi@127.0.0.1>
2023-04-15 07:43:00 +0700

    what

--- domain.py
+++ domain.py
@@ -1,56 +1,24 @@
 import argparse
-import subprocess
-import json
+import os
 
-# Parse the command-line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-d", "--domain", help="Target domain", required=True)
+parser = argparse.ArgumentParser(description='scan a target domain')
+parser.add_argument('-d', '--domain', type=str, required=True, help='target domain to scan')
+
 args = parser.parse_args()
 
-# Get the target domain from the command-line arguments
-target_domain = args.domain
+# Run subfinder and output results to subdomain.txt
+os.system(f"subfinder -d {args.domain} -o subdomain.txt")
 
-# Run Amass to enumerate subdomains
-amass_cmd = f"amass enum -d {target_domain}"
-amass_output = subprocess.check_output(amass_cmd, shell=True)
+# Run dnsgen on subdomain.txt with alternate wordlist and output results to allsubdomain.txt
+os.system(f"dnsgen -w /root/wordlist/dns/alt.txt -l 4 subdomain.txt > allsubdomain.txt")
 
-# Run Subfinder to enumerate subdomains
-subfinder_cmd = f"subfinder -d {target_domain}"
-subfinder_output = subprocess.check_output(subfinder_cmd, shell=True)
+# Combine subfinder and dnsgen output into allsubdomain.txt
+with open('subdomain.txt', 'r') as subfinder_file, open('allsubdomain.txt', 'a') as allsubdomain_file:
+    allsubdomain_file.write(subfinder_file.read())
 
-# Combine the outputs and split by newlines
-all_output = amass_output.decode().strip() + "\n" + 
-subfinder_output.decode().strip()
-all_subdomains = all_output.split("\n")
+with open('allsubdomain.txt', 'a') as allsubdomain_file, open('/root/wordlist/dns/alt.txt', 'r') as alt_wordlist:
+    allsubdomain_file.write(alt_wordlist.read())
 
-# Deduplicate the subdomains and sort them alphabetically
-unique_subdomains = sorted(set(all_subdomains))
+# Resolve all subdomains in allsubdomain.txt with puredns using a list of resolvers and output to subdomain.txt
+os.system(f"puredns resolve allsubdomain.txt -w subdomain.txt -r /root/wordlist/resolvers/resolvers.txt")
 
-# Generate permutation-based subdomains using Dnsgen
-dnsgen_cmd = f"dnsgen -w words.txt -r 5 {target_domain}"
-dnsgen_output = subprocess.check_output(dnsgen_cmd, shell=True)
-
-# Combine the outputs and split by newlines
-all_output = "\n".join(unique_subdomains) + "\n" + 
-dnsgen_output.decode().strip()
-all_subdomains = all_output.split("\n")
-
-# Deduplicate the subdomains and sort them alphabetically
-unique_subdomains = sorted(set(all_subdomains))
-
-# Resolve the subdomains using Puredns
-puredns_cmd = f"puredns resolve -r resolvers.txt -j 10 subdomain.txt"
-puredns_output = subprocess.check_output(puredns_cmd, shell=True)
-
-# Parse the JSON output from Puredns
-puredns_results = json.loads(puredns_output.decode())
-
-# Extract the resolved subdomains from the results
-resolved_subdomains = []
-for result in puredns_results:
-    if result["result"]:
-        resolved_subdomains.append(result["domain"])
-
-# Write the resolved subdomains to a file
-with open("resolved_subdomains.txt", "w") as f:
-    f.write("\n".join(resolved_subdomains))
